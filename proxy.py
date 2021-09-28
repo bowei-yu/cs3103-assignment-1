fcc16be479e20fa309dc4f7c0bf1a2e5c3674b5a
bowei-yu <everydayimboweing@gmail.com>
2021-09-28 14:55:11 +0800

    Add support for other methods

--- proxy.py
+++ proxy.py
@@ -11,8 +11,9 @@ def main():
         proxy_telemetry = int(sys.argv[2])
         proxy_blacklist_path = sys.argv[3]
 
-    except IndexError:
-        print("USAGE: python3 proxy.py <port> <flag_telemetry (0 or 1)> <filename of blacklist>")
+    except IndexError or ValueError:
+        print("USAGE: python3 proxy.py <port (Integer)> <flag_telemetry (0 or 1)> <filename of blacklist (String)>")
+        print("Please ensure that the arguments are in the correct format.")
         return
 
     # set up proxy socket
@@ -30,19 +31,23 @@ def main():
 
 
 class ProxyThread(threading.Thread):
-    
+
+    HTTP_METHODS = {"GET", "CONNECT", "POST", "PUT", "PATCH", "DELETE", "HEAD", "TRACE", "OPTIONS"}
+
+
     def __init__(self, client):
         threading.Thread.__init__(self)
         self.client = client
         self.server = None
+        self.additional_request_data = None
 
 
     # overriden method from threading.Thread library
     def run(self):
-        method, hostname_port, http_version = self.parse_request()
-        print(method, hostname_port, http_version, "\n")
+        method, url, http_version = self.parse_request()
+        print(method, url, http_version, "\n")
         
-        self.handle_request(method, hostname_port, http_version)
+        self.handle_request(method, url, http_version)
 
         self.client.close()
         self.server.close()
@@ -50,56 +55,72 @@ class ProxyThread(threading.Thread):
 
     def parse_request(self):
         request = ""
-    
+
         while True:
             request += self.client.recv(1024).decode('ISO-8859-1')
             end_index = request.find('\r\n')
             
             if end_index > -1:
                 parsed_request = request[:end_index].split(" ")
-                # obtains method, hostname:port and http_version respectively
+                self.additional_request_data = bytes((request[end_index + 1:]), encoding="ISO-8859-1")
+                # obtains method, url and http_version respectively
                 return parsed_request[0], parsed_request[1], parsed_request[2]
 
 
-    def handle_request(self, method, hostname_port, http_version):
+    def handle_request(self, method, url, http_version):
+        
+        if method not in self.HTTP_METHODS:
+            print("Method is not supported!")
+            return
+
         if method == "CONNECT":
-            
-            parsed_address = hostname_port.split(":")
+            parsed_address = url.split(":")
             server_hostname = parsed_address[0]
             server_port = parsed_address[1]
 
-            server_info = socket.getaddrinfo(server_hostname, server_port)
-            address_family = server_info[0][0]
-            server_address = server_info[0][4]
-            # print(address_family, server_address, "\n")
-
-            self.server = socket.socket(address_family)
-            self.server.connect(server_address)
-            self.client.send(bytes((http_version + " 200 Connection established\r\n\r\n"), encoding="ISO-8859-1"))
-
-            # classify into readable, writable and check for exception lists
-            readable_list = [self.client, self.server]
-            writable_list = []
-            exceptional_list = [self.client, self.server]
-
-            while True:
-                is_readable_list, is_writable_list, has_error_list = select.select(readable_list, writable_list, exceptional_list)
-                if len(has_error_list) > 0:
-                    break
-                if len(is_readable_list) == 0:
-                    break
-                for host in is_readable_list:
-                    data = host.recv(1024)
-                    if host == self.server:
-                        sender_host = self.client
-                    else:
-                        sender_host = self.server
-                    if data:
-                        sender_host.send(data)
-
         else:
-            print("handle this later")
-            return
+            parsed_url = url.split("://")[1].split("/")
+            # get the first part of the url (host name)
+            server_hostname = parsed_url[0]
+            # get the path after the hostname eg. /index
+            url = parsed_url[1]
+            server_port = 80
+
+        server_info = socket.getaddrinfo(server_hostname, server_port)
+        address_family = server_info[0][0]
+        server_address = server_info[0][4]
+        # print(address_family, server_address, "\n")
+
+        self.server = socket.socket(address_family)
+        self.server.connect(server_address)
+        if method == "CONNECT":
+            self.client.send(bytes((http_version + " 200 Connection established\r\n\r\n"), encoding="ISO-8859-1"))
+        else:
+            self.server.send(bytes((method + " " + url + " " + http_version), encoding="ISO-8859-1"))
+            self.server.send(self.additional_request_data)
+
+        # classify into readable, writable and check for exception lists
+        readable_list = [self.client, self.server]
+        writable_list = []
+        exceptional_list = [self.client, self.server]
+
+        # interaction between client and server through proxy
+        while True:
+            is_readable_list, is_writable_list, has_error_list = select.select(readable_list, writable_list, exceptional_list)
+            if len(has_error_list) > 0:
+                break
+            if len(is_readable_list) == 0:
+                break
+            for sender_socket in is_readable_list:
+                data = sender_socket.recv(1024)
+                if sender_socket == self.server:
+                    receiver_socket = self.client
+                else:
+                    receiver_socket = self.server
+                if data:
+                    receiver_socket.send(data)
+                else:
+                    break
 
 
 
